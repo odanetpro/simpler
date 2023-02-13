c8d150ab0b966380da64337df709d1a743742b8f
odanetpro <odanetpro@gmail.com>
2023-02-13 13:55:28 +0900

    fix: fix route methods

--- lib/simpler/router/route.rb
+++ lib/simpler/router/route.rb
@@ -11,17 +11,24 @@ module Simpler
       end
 
       def match?(method, path)
-        clear_path = path.delete_suffix('/')
+        clear_path = remove_extra_slashes(path)
 
-        @method == method && clear_path.match(path_regexp).to_s == clear_path
+        @method == method && clear_path.match(path_regexp).to_a[0] == clear_path
       end
 
       def params(path)
-        params = path.match(path_regexp)&.named_captures || {}
+        clear_path = remove_extra_slashes(path)
+
+        params = clear_path.match(path_regexp)&.named_captures || {}
         params.transform_keys { |key| key.delete_prefix(':').to_sym }
       end
 
       private
+
+      def remove_extra_slashes(path)
+        result = path.gsub(%r{/+}, '/').delete_suffix('/')
+        result == '' ? '/' : result
+      end
 
       def path_regexp
         @path.gsub(/(:\w+)/, '(?<\0>\\w+)')
